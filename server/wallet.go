14d1e70318fc31d1e3f7e3891c7046d15705bc5d
Manish jha <devmanjha@gmail.com>
2025-07-06 01:34:38 +0530

    wallet client fixes

--- server/wallet.go
+++ server/wallet.go
@@ -16,19 +16,21 @@ type walletRequest struct {
 
 func getWalletHandler(w http.ResponseWriter, r *http.Request) {
 	userID := r.Context().Value("userID").(string)
-	ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
-	defer cancel()
-	var user User
-	err := db.Collection("users").FindOne(ctx, bson.M{"_id": primitive.ObjectIDFromHex(userID)}).Decode(&user)
+	objID, err := primitive.ObjectIDFromHex(userID)
 	if err != nil {
-		http.Error(w, "not found", http.StatusNotFound)
-		return
-	}
-	json.NewEncoder(w).Encode(user.Wallet)
+    	http.Error(w, "invalid user id", http.StatusBadRequest)
+    	return
+	}	
+	err = db.Collection("users").FindOne(ctx, bson.M{"_id": objID}).Decode(&user)
 }
 
 func depositHandler(w http.ResponseWriter, r *http.Request) {
 	userID := r.Context().Value("userID").(string)
+	objID, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		http.Error(w, "invalid user id", http.StatusBadRequest)
+		return
+	}
 	var req walletRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "bad request", http.StatusBadRequest)
@@ -37,15 +39,21 @@ func depositHandler(w http.ResponseWriter, r *http.Request) {
 
 	ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
 	defer cancel()
-	_, err := db.Collection("users").UpdateOne(ctx, bson.M{"_id": primitive.ObjectIDFromHex(userID)}, bson.M{"$inc": bson.M{"wallet.balance": req.Amount}})
+	_, err = db.Collection("users").UpdateOne(ctx, bson.M{"_id": objID}, bson.M{"$inc": bson.M{"wallet.balance": req.Amount}})
 	if err != nil {
 		http.Error(w, "db error", http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusNoContent)
 }
+
 func withdrawHandler(w http.ResponseWriter, r *http.Request) {
 	userID := r.Context().Value("userID").(string)
+	objID, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		http.Error(w, "invalid user id", http.StatusBadRequest)
+		return
+	}
 	var req walletRequest
 	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
 		http.Error(w, "bad request", http.StatusBadRequest)
@@ -54,7 +62,7 @@ func withdrawHandler(w http.ResponseWriter, r *http.Request) {
 
 	ctx, cancel := context.WithTimeout(r.Context(), 5*time.Second)
 	defer cancel()
-	_, err := db.Collection("users").UpdateOne(ctx, bson.M{"_id": primitive.ObjectIDFromHex(userID)}, bson.M{"$inc": bson.M{"wallet.balance": -req.Amount}})
+	_, err = db.Collection("users").UpdateOne(ctx, bson.M{"_id": objID}, bson.M{"$inc": bson.M{"wallet.balance": -req.Amount}})
 	if err != nil {
 		http.Error(w, "db error", http.StatusInternalServerError)
 		return
